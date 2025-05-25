df451b27abe30f8210f4d35e75bc4575686212c0
otwjunior <jamesthebark@proton.me>
2025-05-25 21:27:42 +0300

    remove floats

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses: "))
-savings = income - expenses
-projected_savings = savings * 12 + (savings * 12 * 0.05)
-print(f"Your monthly savings are ${int(savings)}.")
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = income - expenses
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"Your monthly savings are ${int(monthly_savings)}.")
 print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
