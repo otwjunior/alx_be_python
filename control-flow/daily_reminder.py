45c5ce28e4d65673b3450154d5c9e9b9e211b6f3
otwjunior <jamesthebark@proton.me>
2025-05-29 10:05:21 +0300

    personal daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,21 +1,21 @@
 #todo list
 task = input('Enter your task: ')
 priority = input('Priority (high/medium/low): ')
-time = input('Is it time-bound? (yes/no): ')
+time_bound = input('Is it time-bound? (yes/no): ')
 
 match priority:
     case 'high':
-        if time == 'yes':
+        if time_bound == 'yes':
             print('Reminder: ' +task + ' is important and urgent, complete it now.')
         else:
             print('Reminder: '+task +' require immediate attention today.')
     case 'medium':
-        if time == 'yes':
+        if time_bound == 'yes':
             print('Alert:'+task + ' is important complete within 24 hours.')
         else:
             print('Check '+ task+ ' need to be complete within 48 hours.')
     case 'low':
-        if time =='yes':
+        if time_bound =='yes':
             print('Note:'+task +'  not urgent do it before weekend.')
         else:
             print('Note: '+task+' can be completed on weekend.')
