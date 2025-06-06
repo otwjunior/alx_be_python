7be39f1562eb34560525353929615d9a2dc03636
otwjunior <jamesthebark@proton.me>
2025-06-06 11:08:49 +0300

    arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1,num2,operation):
+    try:
+        if operation=='add':
+            return num1+num2
+        elif operation=='substract':
+            return num1-num2
+        elif operation=='multiply':
+            return num1*num2
+        elif operation=='divide':
+            return num1/num2
+        else:
+            return 'operation not allowed.'
+    except ZeroDivisionError:
+        return 'cannot divide by zero'
