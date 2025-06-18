4f84330701f2d86f191c68107d8abaabbee12bd9
otwjunior <jamesthebark@proton.me>
2025-06-18 12:16:14 +0300

    error proof calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,8 @@
 def safe_divide(numerator, denominator):
     try:
-        return float(numerator) / float(denominator)
+        answer=float(numerator) / float(denominator)
+        return f'The result of the division is {answer}'
     except ValueError:
         return 'Error: Please enter numeric values only.'
     except ZeroDivisionError:
-        return' Error: Cannot divide by zero.'
+        return 'Error: Cannot divide by zero.'
