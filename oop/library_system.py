dfa767274e4dc49a7d3e94706dd07444f2eacee1
otwjunior <jamesthebark@proton.me>
2025-06-26 16:44:25 +0300

    inheritance and composition

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,28 +2,45 @@ class Book:
     def __init__(self,title,author):
         self.title = title
         self.author = author
-
+    def __str__(self):
+        return f'Book: {self.title} by {self.author}'
 
 class EBook(Book):
     def __init__(self,title,author,file_size):
         self.file_size = file_size
         super().__init__(title,author)
+    def __str__(self):
+        return f'{self.title} by {self.author}, File Size:{self.file_size}'
 class PrintBook(Book):
     def __init__(self,title,author,page_count):
         super().__init__(title,author)
         self.page_count = page_count
+    def __str__(self):
+        return f'PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}'
 class Library:
-    def __init__(self, books=[]):
+    def __init__(self, books=None):
+        if books is None:
+            books = []#an empty  list of books
         self.books = books
     
     def add_book(self, book):
         self.books.append(book)
 
+
     def list_books(self):
-        for book in self.books:
-            if isinstance(book, EBook):
-                print(f'{book.title} by {book.author}, File Size:{book.file_size}')
-            elif isinstance(book,PrintBook):
-                print(f'PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}')
-            print(f'Book: {book.title} by {book.author}')
+        for book in self.books:                
+            print(book)
+class Library:
+    def __init__(self, books=None):
+        if books is None:
+            books = []
+        self.books = books
+    
+    def add_book(self, book):
+        self.books.append(book)
+
+
+    def list_books(self):
+        for book in self.books:                
+            print(book)
     
