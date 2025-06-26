6e15f9254d04d66bbdab4fa508fc9e85b76b5b53
otwjunior <jamesthebark@proton.me>
2025-06-26 17:03:04 +0300

    method override

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -10,7 +10,7 @@ class Rectangle(Shape):
         self.width =width
     
     def area(self):
-        return self.width  *self.length
+        return self.width * self.length
 class Circle(Shape):
     def __init__(self,radius):
         self.radius = radius
